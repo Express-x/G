09c3762f0f8b0768d22c867659ddfa3cf965de42
Express-x <130709513+Express-x@users.noreply.github.com>
2024-09-19 17:04:07 +0000

    gggg

--- layers.py
+++ layers.py
@@ -5,7 +5,7 @@ class Layer:
         self.input = None
         self.output = None
 
-    def forward_propagation(self, input):
+    def forward_propagation(self, input_data):
         raise NotImplementedError
 
     def backward_propagation(self, output_error, learning_rate):
@@ -13,10 +13,26 @@ class Layer:
 
 
 class FCLayer(Layer):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, weight_initializer='xavier', bias_initializer='zeros'):
         super().__init__()
-        self.weights = np.random.rand(input_size, output_size) - 0.5
-        self.bias = np.random.rand(1, output_size) - 0.5
+        self.weights = self._initialize_weights(input_size, output_size, weight_initializer)
+        self.bias = self._initialize_bias(output_size, bias_initializer)
+
+    def _initialize_weights(self, input_size, output_size, initializer):
+        if initializer == 'xavier':
+            scale = np.sqrt(2 / (input_size + output_size))
+            return np.random.normal(0, scale, (input_size, output_size))
+        elif initializer == 'he':
+            scale = np.sqrt(2 / input_size)
+            return np.random.normal(0, scale, (input_size, output_size))
+        else:
+            return np.random.rand(input_size, output_size) - 0.5
+
+    def _initialize_bias(self, output_size, initializer):
+        if initializer == 'zeros':
+            return np.zeros((1, output_size))
+        else:
+            return np.random.rand(1, output_size) - 0.5
 
     def forward_propagation(self, input_data):
         self.input = input_data
@@ -53,38 +69,38 @@ class InputLayer(Layer):
 
     def forward_propagation(self, input_data):
         self.input = input_data
-        self.output = input_data  # Identity function for input layer
+        self.output = input_data
         return self.output
 
     def backward_propagation(self, output_error, learning_rate):
-        return output_error  # No change needed for input layer
+        return output_error
 
 
 class HiddenLayer(FCLayer):
-    def __init__(self, input_size, output_size):
-        super().__init__(input_size, output_size)
+    def __init__(self, input_size, output_size, weight_initializer='xavier', bias_initializer='zeros'):
+        super().__init__(input_size, output_size, weight_initializer, bias_initializer)
 
 
 class OutputLayer(FCLayer):
-    def __init__(self, input_size, output_size):
-        super().__init__(input_size, output_size)
+    def __init__(self, input_size, output_size, weight_initializer='xavier', bias_initializer='zeros'):
+        super().__init__(input_size, output_size, weight_initializer, bias_initializer)
 
 
 class DenseLayer(FCLayer):
-    def __init__(self, input_size, output_size):
-        super().__init__(input_size, output_size)
+    def __init__(self, input_size, output_size, weight_initializer='xavier', bias_initializer='zeros'):
+        super().__init__(input_size, output_size, weight_initializer, bias_initializer)
 
 
 class ReceptronLayer(FCLayer):
-    def __init__(self, input_size):
-        super().__init__(input_size, 1)
+    def __init__(self, input_size, weight_initializer='xavier', bias_initializer='zeros'):
+        super().__init__(input_size, 1, weight_initializer, bias_initializer)
 
 
 def tanh(x):
     return np.tanh(x)
 
 def tanh_prime(x):
-    return 1-np.tanh(x)**2
+    return 1 - np.tanh(x) ** 2
 
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
@@ -98,4 +114,3 @@ def relu(x):
 
 def relu_prime(x):
     return np.where(x > 0, 1, 0)
-
